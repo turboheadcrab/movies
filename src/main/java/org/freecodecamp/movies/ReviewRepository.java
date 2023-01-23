d0c09035f51cc78e5f6c6a204789b25d69d47408
Vladimir Apraksin (OSV) <vladimir.apraksin.osv@fedex.com>
2023-01-23 00:22:28 -0500

    Movies and Reviews endpoints

---
+++ src/main/java/org/freecodecamp/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package org.freecodecamp.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
