ee29cc524b8aabe014ce92c13b645659dcf32b1a
Vladimir Apraksin (OSV) <vladimir.apraksin.osv@fedex.com>
2023-01-24 13:46:19 -0500

    Implemented the Use State and Use Effect Hooks

--- src/main/java/org/freecodecamp/movies/MoviesApplication.java
+++ src/main/java/org/freecodecamp/movies/MoviesApplication.java
@@ -2,6 +2,9 @@ package org.freecodecamp.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -10,4 +13,17 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 }
